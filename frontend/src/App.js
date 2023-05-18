5c2b41de5df1a2c7e0eaf786a3370e85440095b1
Shawn Zhuang <cubershz@gmail.com>
2023-05-17 17:30:32 -0700

    Add margins to heading

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -9,15 +9,12 @@ import Home from './pages/Home';
 
 function App() {
   return (
-    <h1 className="hidden">
-      Hello world!
-    </h1>
-    // <Router>
-    //   <Routes>
-    //     <Route exact path="/" element={<Home/>}/>
-    //     <Route exact path="/rides" element={<Rides/>}/>
-    //   </Routes>
-    // </Router>
+    <Router>
+      <Routes>
+        <Route exact path="/" element={<Home/>}/>
+        <Route exact path="/rides" element={<Rides/>}/>
+      </Routes>
+    </Router>
   );
 }
 
--- frontend/src/pages/Home.js
+++ frontend/src/pages/Home.js
@@ -2,10 +2,10 @@ import './Home.css';
 
 function Home() {
   return (
-    <div>
-        <div className="hidden">
-            <h1>Bruber</h1>
-            <h2>Ridesharing for Bruins</h2>
+    <div className='home-container'>
+        <div className="home-heading">
+            <h1 className='home-heading-title'>Bruber</h1>
+            <h2 className='home-heading-description'>Ridesharing for Bruins</h2>
         </div>
         <h3>Find other UCLA students and split the cost:</h3>
     </div>
