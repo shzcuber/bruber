1af9600184f1ef8bfadd3c575ef94b2927724d6d
Kevin1729 <kxia1729@gmail.com>
2023-05-18 17:39:49 -0700

    Put each ride into separate cards

--- frontend/src/pages/Rides.js
+++ frontend/src/pages/Rides.js
@@ -1,22 +1,43 @@
+import {
+    SimpleGrid,
+    VStack,
+    Heading,
+    Card,
+    CardHeader,
+    CardBody,
+    CardFooter,
+    Text,
+    ListItem,
+    UnorderedList,
+} from "@chakra-ui/react";
 function RideCard(props) {
     //props will have a time, a list of names that are part of the carpool group, and a carpool capacity (will fill any vacancies with "empty")
     console.log(props);
-    let emptyList = Array(props.capacity).fill("Empty");
+    let emptyList = Array(props.capacity - props.names.length).fill("Empty");
     const peopleList = [...props.names, ...emptyList].map((name, index) => {
         return (
-            <li key={index}>
+            <ListItem key={index}>
                 <h3>{name}</h3>
-            </li>
+            </ListItem>
         );
     });
     return (
-        <div>
-            <h2>{props.time}</h2>
-            <ol>{peopleList}</ol>
-            <h3>
-                {props.names.length + "/" + props.capacity + " Spots Available"}
-            </h3>
-        </div>
+        <Card>
+            <CardHeader></CardHeader>
+            <CardBody>
+                <VStack spacing={4} align="left">
+                    <Heading size="md">{props.time}</Heading>
+                    <UnorderedList paddingLeft={15}>{peopleList}</UnorderedList>
+                    <Text as="b">
+                        {props.names.length +
+                            "/" +
+                            props.capacity +
+                            " Spots Available"}
+                    </Text>
+                </VStack>
+            </CardBody>
+            <CardFooter></CardFooter>
+        </Card>
     );
 }
 function Rides() {
@@ -24,26 +45,31 @@ function Rides() {
         <div>
             <h1>Rides</h1>
             <h2>{"LAX -> UCLA on June 15, 2029"}</h2>
-            <RideCard
-                time="9:00"
-                capacity={4}
-                names={["Joe Mama", "Hugh Janis"]}
-            />
-            <RideCard
-                time="9:00"
-                capacity={4}
-                names={["Joe Mama", "Hugh Janis"]}
-            />
-            <RideCard
-                time="9:00"
-                capacity={4}
-                names={["Joe Mama", "Hugh Janis"]}
-            />
-            <RideCard
-                time="9:00"
-                capacity={4}
-                names={["Joe Mama", "Hugh Janis"]}
-            />
+            <SimpleGrid
+                spacing={4}
+                templateColumns="repeat(auto-fill, minmax(200px,1fr))"
+            >
+                <RideCard
+                    time="9:00"
+                    capacity={4}
+                    names={["Joe Mama", "Hugh Janis"]}
+                />
+                <RideCard
+                    time="9:00"
+                    capacity={4}
+                    names={["Joe Mama", "Hugh Janis"]}
+                />
+                <RideCard
+                    time="9:00"
+                    capacity={4}
+                    names={["Joe Mama", "Hugh Janis"]}
+                />
+                <RideCard
+                    time="9:00"
+                    capacity={4}
+                    names={["Joe Mama", "Hugh Janis"]}
+                />
+            </SimpleGrid>
         </div>
     );
 }
