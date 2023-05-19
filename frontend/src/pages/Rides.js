343c3983871086f6c09234b28f9da621c3070628
Kevin1729 <kxia1729@gmail.com>
2023-05-18 19:35:31 -0700

    Pad ride cards, add Avatar to card headers

--- frontend/src/pages/Rides.js
+++ frontend/src/pages/Rides.js
@@ -1,4 +1,6 @@
 import {
+    Avatar,
+    Box,
     SimpleGrid,
     VStack,
     Heading,
@@ -11,25 +13,39 @@ import {
     UnorderedList,
 } from "@chakra-ui/react";
 function RideCard(props) {
-    //props will have a time, a list of names that are part of the carpool group, and a carpool capacity (will fill any vacancies with "empty")
+    /* props will have:
+     * a driver,
+     * a time,
+     * a list of names that are part of the carpool group,
+     * a carpool capacity (will fill any vacancies with "empty")
+     */
     console.log(props);
     let emptyList = Array(props.capacity - props.names.length).fill("Empty");
     const peopleList = [...props.names, ...emptyList].map((name, index) => {
         return (
-            <ListItem key={index}>
-                <h3>{name}</h3>
+            <ListItem key={index} paddingLeft="15px">
+                <Text fontSize="2xl">{name}</Text>
             </ListItem>
         );
     });
     return (
-        <Card>
-            <CardHeader></CardHeader>
+        <Card padding="30px">
+            <CardHeader>
+                <Avatar name={props.driver} />
+            </CardHeader>
             <CardBody>
                 <VStack spacing={4} align="left">
-                    <Heading size="md">{props.time}</Heading>
-                    <UnorderedList paddingLeft={15}>{peopleList}</UnorderedList>
-                    <Text as="b">
-                        {props.names.length +
+                    <Heading size="xl">{props.time}</Heading>
+                    <UnorderedList
+                        paddingLeft="25px"
+                        height="175px"
+                        overflow="hidden"
+                    >
+                        {peopleList}
+                    </UnorderedList>
+                    <Text as="b" fontSize="2xl">
+                        {props.capacity -
+                            props.names.length +
                             "/" +
                             props.capacity +
                             " Spots Available"}
@@ -42,17 +58,29 @@ function RideCard(props) {
 }
 function Rides() {
     return (
-        <div>
-            <h1>Rides</h1>
-            <h2>{"LAX -> UCLA on June 15, 2029"}</h2>
+        <Box padding="40px">
+            <Heading as="h1" size="4xl">
+                Rides
+            </Heading>
+            <Heading as="h2" size="xl" margin="20px 0px">
+                {"LAX -> UCLA on June 15, 2029"}
+            </Heading>
             <SimpleGrid
-                spacing={4}
-                templateColumns="repeat(auto-fill, minmax(200px,1fr))"
+                columns={2}
+                spacingX="40px"
+                spacingY="20px"
+                margin="30px 35px"
             >
                 <RideCard
+                    driver="Joe Biden"
                     time="9:00"
-                    capacity={4}
-                    names={["Joe Mama", "Hugh Janis"]}
+                    capacity={14}
+                    names={[
+                        "Joe Biden",
+                        "Barack Obama",
+                        "Donald Trump",
+                        "Drake",
+                    ]}
                 />
                 <RideCard
                     time="9:00"
@@ -70,7 +98,7 @@ function Rides() {
                     names={["Joe Mama", "Hugh Janis"]}
                 />
             </SimpleGrid>
-        </div>
+        </Box>
     );
 }
 
