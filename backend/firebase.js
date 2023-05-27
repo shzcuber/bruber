5d1808dffd769fd78eacbfc67bb437575799131f
Viraj cz <virajchhajed21@gmail.com>
2023-05-26 20:14:30 -0700

    backend setup

---
+++ backend/firebase.js
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "",
+  authDomain: "bruber-b655f.firebaseapp.com",
+  projectId: "bruber-b655f",
+  storageBucket: "bruber-b655f.appspot.com",
+  messagingSenderId: "954731036862",
+  appId: "1:954731036862:web:2260aa785228d5e4d1deef"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
