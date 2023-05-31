485cf2fc58578e381d82fa947027f5d9e5cdb965
petern <petern0408@ucla.edu>
2023-05-30 17:10:45 -0700

    Add success status message for CreateRide.js

--- frontend/src/pages/CreateRide.js
+++ frontend/src/pages/CreateRide.js
@@ -1,65 +1,87 @@
 import { Button, Input, Box, Card, CardHeader, CardBody, Heading, 
     Stack, StackDivider, Flex, Alert, AlertIcon, NumberInput, 
     NumberInputField, NumberInputStepper, NumberIncrementStepper,
-    NumberDecrementStepper } 
+    NumberDecrementStepper, FormControl, FormLabel }
     from "@chakra-ui/react";
 
 import { useState } from 'react';
 
 
 function CreateRide() {
+  const [submitted, setSubmitted] = useState(false);
+
+  function displayAlert(status) {
+    console.log(status);
+    return (
+      <Box>
+        <Alert status={status}>
+          <AlertIcon />
+          Ride successfully created!
+        </Alert>
+      </Box>
+    );
+  }
+  if (submitted !== false) { // REMOVE !
+    // else if error status="error"
+  }
   return (
-    <div>
-    <Box display="flex" justifyContent="center" alignItems="center" height="100vh">
+    <Box>
+      {
+      submitted ? displayAlert(submitted) : <Box></Box>
+      }
+      <Box display="flex" justifyContent="center" alignItems="center" height="100vh">
       <Card size="lg" borderRadius="20px">
         <CardHeader>
           <Heading size='md'>Create a New Ride</Heading>
         </CardHeader>
         <CardBody>
           <Stack divider={<StackDivider />} spacing='4'>
-            <Box>
-              <Heading size='xs' marginBottom="2" textTransform='uppercase'>
-                From:
-              </Heading>
-              <Input placeholder="City" size="sm" />
-            </Box>
-            <Box>
-              <Heading size='xs' marginBottom="2" textTransform='uppercase'>
-                To:
-              </Heading>
-              <Input placeholder="City" size="sm" />
-            </Box>
-            <Box>
-              <Heading size='xs' marginBottom="2" textTransform='uppercase'>
-                Capacity
-              </Heading>
-              <NumberInput defaultValue={1} min={1} size="sm">
-                <NumberInputField />
-                <NumberInputStepper>
-                  <NumberIncrementStepper />
-                  <NumberDecrementStepper />
-                </NumberInputStepper>
-              </NumberInput>
-            </Box>
-            <Box>
-              <Heading size='xs' marginBottom="2" textTransform='uppercase'>
-                Leave Time:
-              </Heading>
-              <Flex>
-                <Input type="datetime-local" size="sm" />
-                <Button type="submit" size="s" fontSize='sm' padding="2" 
-                marginLeft="2"
-                onSubmit={ handleButtonClick }>
-                  Submit
-                </Button>
-              </Flex>
-            </Box>
+            <FormControl isRequired>
+              <Box>
+                <FormLabel size='xs' textTransform='uppercase'>
+                  From:
+                </FormLabel>
+                <Input placeholder="City" size="sm" />
+              </Box>
+              <Box>
+                <FormLabel size='xs' textTransform='uppercase'>
+                  To:
+                </FormLabel>
+                <Input placeholder="City" size="sm" />
+              </Box>
+              <Box>
+                <FormLabel size='xs' textTransform='uppercase'>
+                  Capacity
+                </FormLabel>
+                <NumberInput defaultValue={1} min={1} size="sm">
+                  <NumberInputField />
+                  <NumberInputStepper>
+                    <NumberIncrementStepper />
+                    <NumberDecrementStepper />
+                  </NumberInputStepper>
+                </NumberInput>
+              </Box>
+              <Box>
+                <FormLabel size='xs' marginBottom="2" textTransform='uppercase'>
+                  Leave Time:
+                </FormLabel>
+                <Flex>
+                  <Input type="datetime-local" size="sm" />
+                  <Button type="submit" size="s" fontSize='sm' padding="2"
+                  marginLeft="2"
+                  onClick={ handleButtonClick }>
+                    Submit
+                  </Button>
+                </Flex>
+              </Box>
+            </FormControl>
           </Stack>
         </CardBody>
       </Card>
+      </Box>
     </Box>
-  </div>
   );
+
 
 
 }
