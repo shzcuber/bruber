c1094847cae5f1dce31723e6453b126004079a64
Kevin1729 <kxia1729@gmail.com>
2023-06-03 22:47:18 -0700

    Make timestamp pretty on search bar

--- frontend/src/pages/Rides.js
+++ frontend/src/pages/Rides.js
@@ -264,7 +264,7 @@ function Rides() {
       </Flex>
       <Flex align="end">
         <Heading as="h2" size="md" margin="20px 0px">
-          {start + " to " + destination + ", departing on " + time}
+          {start + " to " + destination + ", departing on " + parseTime(time)}
         </Heading>
       </Flex>
       <Box marginBottom="20px">
@@ -304,4 +304,48 @@ function passengersToList(passengers, capacity) {
     );
   });
   return peopleList;
+}
+
+function parseTime(time) {
+  const monthNames = [
+    "",
+    "January",
+    "February",
+    "March",
+    "April",
+    "May",
+    "June",
+    "July",
+    "August",
+    "September",
+    "October",
+    "November",
+    "December",
+  ];
+  const timeArray = time.split("-");
+  const dayTime = timeArray[2].split("T");
+  const day = dayTime[0];
+  const timeOfDay = dayTime[1];
+  const hourMinutes = timeOfDay.split(":");
+  let hour = hourMinutes[0];
+  const minutes = hourMinutes[1];
+  let half = "AM"; //AM or PM
+  if (hour > 12) {
+    hour -= 12;
+    half = "PM";
+  }
+  if (hour === 0) hour = 12;
+  return (
+    monthNames[parseInt(timeArray[1])] +
+    " " +
+    day +
+    ", " +
+    timeArray[0] +
+    " at " +
+    hour +
+    ":" +
+    minutes +
+    " " +
+    half
+  );
 }
