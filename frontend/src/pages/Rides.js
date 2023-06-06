70d94fbbd39844c6e377797a2eb825a625b03065
Shawn Zhuang <cubershz@gmail.com>
2023-06-06 00:43:04 -0700

    Move RideCardAccordian to separate file

--- frontend/src/pages/Rides.js
+++ frontend/src/pages/Rides.js
@@ -36,6 +36,7 @@ import JourneyInputter from "../components/JourneyInputter";
 
 import RideSignupButton from "../components/RideSignupButton";
 import Navbar from "../components/Navbar";
+import RideCardAccordion from "../components/RideCardAccordian";
 
 import { getAuth } from "firebase/auth";
 
@@ -63,71 +64,6 @@ const sampleRideInfo = {
 
 const sampleLocations = ["LAX", "UCSD", "UCI", "UCR", "UCB", "UCSB", "UCLA"];
 
-function RideCardAccordion(props) {
-  const rideAccordionItems = props.rides.map((ride, index) => (
-    <AccordionItem
-      key={index}
-      background="white"
-      color="primary.500"
-      borderColor="primary.500"
-    >
-      <AccordionButton
-        display="block"
-        _expanded={{ bg: "primary.500", color: "white" }}
-      >
-        <Flex align="center">
-          <Flex gap="4" align="center">
-            <Avatar name={ride.driverFirstName} />
-            <Box textAlign="left">
-              <Text as="b" fontSize="xl">
-                {ride.driverFirstName}
-              </Text>
-              <Text fontSize="xl">Driver</Text>
-            </Box>
-          </Flex>
-          <Spacer />
-          <AccordionIcon />
-        </Flex>
-      </AccordionButton>
-      <AccordionPanel
-        padding="0px"
-        background="primary.150"
-        color="primary.500"
-      >
-        <Box margin="15px">
-          <Text p="10px" fontWeight="bold" fontSize="xl">
-            {parseTime(ride.startTime)}
-          </Text>
-          <Text p="10px" fontWeight="bold" fontSize="xl">
-            From: {ride.from}
-          </Text>
-          <Text p="10px" fontWeight="bold" fontSize="xl">
-            To: {ride.to}
-          </Text>
-          <UnorderedList paddingLeft="25px" height="150px" overflow="hidden">
-            {passengersToList(ride.passengers, ride.capacity)}
-          </UnorderedList>
-          <Text as="b" fontSize="2xl" paddingLeft="15px">
-            {ride.capacity -
-              ride.passengers.length +
-              "/" +
-              ride.capacity +
-              " Spots Available"}
-          </Text>
-          <Box margin="25px 15px">
-            <RideSignupButton authUser={props.authUser} rideId={ride.id} />
-          </Box>
-        </Box>
-      </AccordionPanel>
-    </AccordionItem>
-  ));
-
-  return (
-    <Accordion allowMultiple margin="20px 0px">
-      {rideAccordionItems}
-    </Accordion>
-  );
-}
 function RidesDisplay(props) {
   const [viewAccordion, setViewAccordion] = useControllableState({
     defaultValue: true,
---
+++ frontend/src/pages/UpcomingRides.js
@@ -0,0 +1,9 @@
+import { Box } from "@chakra-ui/react"
+import Navbar from "../components/Navbar"
+
+export default function UpcomingRides(props)
+{
+    return (<Box>
+                <Navbar />
+            </Box>)
+}
