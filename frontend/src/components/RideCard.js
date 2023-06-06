3947302ca5d82fa4e559901f568b6fab81b2b78c
Kevin1729 <kxia1729@gmail.com>
2023-06-06 03:03:41 -0700

    Print stars next to name on Ride cards

--- frontend/src/components/RideCard.js
+++ frontend/src/components/RideCard.js
@@ -15,7 +15,7 @@ import RideSignupButton from "./RideSignupButton";
 import { useDisclosure } from "@chakra-ui/react";
 
 import RatingsModal from "./RatingsModal";
-import { passengersToList } from "../utilities";
+import { passengersToList, getStarString } from "../utilities";
 
 export default function RideCard(props) {
   /* props will have:
@@ -27,18 +27,23 @@ export default function RideCard(props) {
   // console.log(props);
   const peopleList = passengersToList(props.names, props.capacity);
   const { isOpen, onOpen, onClose } = useDisclosure();
-  const capacityReached = (props.capacity - props.names.length) == 0;
+  const capacityReached = props.capacity - props.names.length === 0;
   return (
     <Card variant="rideCard">
       <CardHeader>
-        <Flex gap="4" align="center">
-          <Avatar name={props.driver} />
-          <Box>
-            <Text as="b" fontSize="xl">
-              {props.driver}
-            </Text>
-            <Text fontSize="xl">Driver</Text>
-          </Box>
+        <Flex align="center" justify="space-between">
+          <Flex gap="4" align="center">
+            <Avatar name={props.driver} />
+            <Box textAlign="left">
+              <Text as="b" fontSize="xl">
+                {props.driver}
+              </Text>
+              <Text fontSize="xl">Driver</Text>
+            </Box>
+          </Flex>
+          <Text color="gold" fontSize="xl" mr="25%">
+            {props.rating ? getStarString(props.rating) : "★★★☆☆"}
+          </Text>
         </Flex>
       </CardHeader>
       <CardBody>
@@ -62,14 +67,18 @@ export default function RideCard(props) {
               props.capacity +
               " Spots Available"}
           </Text>
-          {!capacityReached && !props.hideSignupButton && <RideSignupButton authUser={props.authUser} rideId={props.rideId} />}
+          {!capacityReached && !props.hideSignupButton && (
+            <RideSignupButton authUser={props.authUser} rideId={props.rideId} />
+          )}
         </VStack>
       </CardBody>
       <CardFooter>
-        {props.displayRatingButton && <Button onClick={onOpen} colorScheme="secondary">
-          Rate Driver
-        </Button>}
-        <RatingsModal isOpen={isOpen} onClose={onClose}/>
+        {props.displayRatingButton && (
+          <Button onClick={onOpen} colorScheme="secondary">
+            Rate Driver
+          </Button>
+        )}
+        <RatingsModal isOpen={isOpen} onClose={onClose} />
       </CardFooter>
     </Card>
   );
