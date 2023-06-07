4e4aa64bbf3ba23bd42c1d8fa6ca53902a2d1036
Kevin1729 <kxia1729@gmail.com>
2023-06-07 11:14:50 -0700

    Make card view have two columns again

--- frontend/src/components/RideCardGrid.js
+++ frontend/src/components/RideCardGrid.js
@@ -5,30 +5,27 @@ import { parseTime } from "../utilities";
 export default function RideCardGrid(props) {
   // console.log(props.rides[1].driverID)
   const rideCardList = props.rides.map((ride, index) => {
-    console.log(ride, props.rides)
+    console.log(ride, props.rides);
     return (
-    <RideCard
-      key={index}
-      driver={ride.driverFirstName}
-      time={parseTime(ride.startTime)}
-      from={ride.from}
-      driverID={ride.driverID._key.path.segments[6]}
-      to={ride.to}
-      capacity={ride.capacity}
-      names={ride.passengers}
-      rideId={ride.id}
-      rating={ride.rating}
-      hideSignupButton={props.hideSignupButton}
-      authUser={props.authUser}
-      displayRatingButton={props.displayRatingButton}
-    />
-  )});
+      <RideCard
+        key={index}
+        driver={ride.driverFirstName}
+        time={parseTime(ride.startTime)}
+        from={ride.from}
+        driverID={ride.driverID._key.path.segments[6]}
+        to={ride.to}
+        capacity={ride.capacity}
+        names={ride.passengers}
+        rideId={ride.id}
+        rating={ride.rating}
+        hideSignupButton={props.hideSignupButton}
+        authUser={props.authUser}
+        displayRatingButton={props.displayRatingButton}
+      />
+    );
+  });
   return (
-    <SimpleGrid
-      spacingX="40px"
-      spacingY="20px"
-      margin="2% 4%"
-    >
+    <SimpleGrid spacingX="40px" spacingY="20px" margin="2% 4%" columns={2}>
       {rideCardList}
     </SimpleGrid>
   );
