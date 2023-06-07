c833fa0c9a81c60acdf55b76528a3b4a14411f15
Kevin1729 <kxia1729@gmail.com>
2023-06-07 11:20:39 -0700

    Fix bug with minutes displaying without padded 0

--- frontend/src/utilities.js
+++ frontend/src/utilities.js
@@ -60,9 +60,9 @@ export function parseTime(time) {
     ", " +
     timeArray[0] +
     " at " +
-    hour +
+    String(hour).padStart(2, "0") +
     ":" +
-    minutes +
+    String(minutes).padStart(2, "0") +
     " " +
     half
   );
