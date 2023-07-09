24b72d1113bc4d803539af633d88eb732fc12352
Shawn Zhuang <cubershz@gmail.com>
2023-07-08 22:50:47 -0700

    Remove firebase API Key oops

--- frontend/src/pages/auth.js
+++ frontend/src/pages/auth.js
@@ -3,122 +3,162 @@ import { initializeApp } from "firebase/app";
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
 import { useEffect, useState } from "react";
-import {signOut, getAuth, onAuthStateChanged } from "firebase/auth";
+import { signOut, getAuth, onAuthStateChanged } from "firebase/auth";
 import LoginPage from "./Login";
 import Onboarding from "./Onboarding";
 import ForgotPassword from "./ForgotPassword";
-import { BrowserRouter as Router, Routes, Route, Navigate, useNavigate } from "react-router-dom";
-import Home from './Home'
-import Rides from './Rides'
+import {
+  BrowserRouter as Router,
+  Routes,
+  Route,
+  Navigate,
+  useNavigate,
+} from "react-router-dom";
+import Home from "./Home";
+import Rides from "./Rides";
 import Driver from "./Driver";
-import CreateRide from './CreateRide';
-import Profile from './Profile';
-import About from './About';
-import Navbar from "../components/Navbar"
+import CreateRide from "./CreateRide";
+import Profile from "./Profile";
+import About from "./About";
+import Navbar from "../components/Navbar";
 import { list } from "@chakra-ui/react";
 import UpcomingRides from "./UpcomingRides";
 
-
-
 // Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: "AIzaSyBBXKNbip86Utdj3oqoMo7ZtIMjjQffbUI",
+  apiKey: process.env.REACT_APP_API_KEY,
   authDomain: "bruber-b655f.firebaseapp.com",
   projectId: "bruber-b655f",
   storageBucket: "bruber-b655f.appspot.com",
   messagingSenderId: "954731036862",
-  appId: "1:954731036862:web:2260aa785228d5e4d1deef"
+  appId: "1:954731036862:web:2260aa785228d5e4d1deef",
 };
 
 // Initialize Firebase
 export const app = initializeApp(firebaseConfig);
 export const auth = getAuth(app);
 
-
 const AuthDetails = () => {
-    const [authUser, setAuthUser] = useState(null);
-    //initialize states to true to reduce glitchiness
-    const [firstName, setFirstName] = useState(true);
-    const [lastName, setLastName] = useState(true);
-    const [phoneNumber, setPhoneNumber] = useState(true);
+  const [authUser, setAuthUser] = useState(null);
+  //initialize states to true to reduce glitchiness
+  const [firstName, setFirstName] = useState(true);
+  const [lastName, setLastName] = useState(true);
+  const [phoneNumber, setPhoneNumber] = useState(true);
 
+  const refreshUserData = (uid) => {
+    const requestOptions = {
+      method: "GET",
+      headers: { "Content-Type": "application/json" },
+    };
 
-    const refreshUserData = (uid) => {
-        const requestOptions = {
-            method: "GET",
-            headers: { "Content-Type": "application/json" },
-          };
-      
-          fetch(`${process.env.REACT_APP_BACKEND}/user/${uid}`, requestOptions)
-            .then((res) => res.json()) // Convert json to js object
-            .then((data) => {
-                setFirstName(data.firstName);
-                setLastName(data.lastName);
-                setPhoneNumber(data.phoneNumber);
-            })
-            .catch((error) => console.log("Error: " + error));
-    }
+    fetch(`${process.env.REACT_APP_BACKEND}/user/${uid}`, requestOptions)
+      .then((res) => res.json()) // Convert json to js object
+      .then((data) => {
+        setFirstName(data.firstName);
+        setLastName(data.lastName);
+        setPhoneNumber(data.phoneNumber);
+      })
+      .catch((error) => console.log("Error: " + error));
+  };
 
-    useEffect(()=>{
-        onAuthStateChanged(auth, (user) => {
-            if (user) {
-                setAuthUser(user);
-                refreshUserData(user.uid)
-                console.log(user)
-            } else {
-                setAuthUser(null);
-            }
-        });
-    }, [])
+  useEffect(() => {
+    onAuthStateChanged(auth, (user) => {
+      if (user) {
+        setAuthUser(user);
+        refreshUserData(user.uid);
+        console.log(user);
+      } else {
+        setAuthUser(null);
+      }
+    });
+  }, []);
 
-
-    return (
-        <div>
-            <h1>{authUser && authUser.emailVerified ? (
-                firstName && lastName && phoneNumber ? (
-                <Router>
-                <Routes>
-                    <Route exact path="/" element={<Home authUser={authUser} />}/>
-                    <Route exact path="/onboarding" element={<Onboarding authUser={authUser} />}/>
-                    <Route exact path="/navbar" element={<Navbar authUser={authUser}/>}/>
-                    <Route exact path="/rides" element={<Rides authUser={authUser}/>}/>
-                    <Route path="/driver" element={<Driver authUser={authUser}/>} />
-                    <Route exact path="/create_ride" element={<CreateRide authUser={authUser}/>}/>
-                    <Route exact path="/profile" element={<Profile authUser={authUser}/>}/>
-                    <Route exact path="/upcoming_rides" element={<UpcomingRides authUser={authUser}/>}/>
-                    <Route exact path="/about" element={<About authUser={authUser}/>}/>
-                    <Route path="*" element={<Navigate to="/" replace />}/>
-                </Routes>
-                </Router>
-                ) : (
-                    <Router>
-                        <Onboarding authUser={authUser} />
-                    </Router>
-                )
-            ) : (
+  return (
+    <div>
+      <h1>
+        {authUser && authUser.emailVerified ? (
+          firstName && lastName && phoneNumber ? (
             <Router>
-            <Routes>
-                <Route exact path="/" element={<Home authUser={authUser}/>}/>
-                <Route exact path="/forgot-password" element={<ForgotPassword authUser={authUser}/>}/>
-                <Route exact path="/about" element={<About authUser={authUser}/>}/>
-                <Route path="*" element={<LoginPage authUser={authUser}/>}/>
-            </Routes>
+              <Routes>
+                <Route exact path="/" element={<Home authUser={authUser} />} />
+                <Route
+                  exact
+                  path="/onboarding"
+                  element={<Onboarding authUser={authUser} />}
+                />
+                <Route
+                  exact
+                  path="/navbar"
+                  element={<Navbar authUser={authUser} />}
+                />
+                <Route
+                  exact
+                  path="/rides"
+                  element={<Rides authUser={authUser} />}
+                />
+                <Route
+                  path="/driver"
+                  element={<Driver authUser={authUser} />}
+                />
+                <Route
+                  exact
+                  path="/create_ride"
+                  element={<CreateRide authUser={authUser} />}
+                />
+                <Route
+                  exact
+                  path="/profile"
+                  element={<Profile authUser={authUser} />}
+                />
+                <Route
+                  exact
+                  path="/upcoming_rides"
+                  element={<UpcomingRides authUser={authUser} />}
+                />
+                <Route
+                  exact
+                  path="/about"
+                  element={<About authUser={authUser} />}
+                />
+                <Route path="*" element={<Navigate to="/" replace />} />
+              </Routes>
             </Router>
-            )}</h1>
-        </div>
-    )
-}
+          ) : (
+            <Router>
+              <Onboarding authUser={authUser} />
+            </Router>
+          )
+        ) : (
+          <Router>
+            <Routes>
+              <Route exact path="/" element={<Home authUser={authUser} />} />
+              <Route
+                exact
+                path="/forgot-password"
+                element={<ForgotPassword authUser={authUser} />}
+              />
+              <Route
+                exact
+                path="/about"
+                element={<About authUser={authUser} />}
+              />
+              <Route path="*" element={<LoginPage authUser={authUser} />} />
+            </Routes>
+          </Router>
+        )}
+      </h1>
+    </div>
+  );
+};
 
 export const userSignOut = () => {
-    signOut(auth).then(
-        () => {
-            console.log("signed out!")
-        }
-    ).catch(
-        (error) => {
-            console.log("error" + error)
-        }
-    )
-}
+  signOut(auth)
+    .then(() => {
+      console.log("signed out!");
+    })
+    .catch((error) => {
+      console.log("error" + error);
+    });
+};
 
 export default AuthDetails;
